1456437f6edbdb47a0a960826fe9fe5b950baf6f
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-03-03 15:10:33 +0900

    start

---
+++ src/main/java/com/neuyshop/NeuyshopApplication.java
@@ -0,0 +1,13 @@
+package com.neuyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neuyshop/NeuyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
